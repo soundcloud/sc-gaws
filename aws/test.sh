7e6fabf80d8f5eccf02fc724b0d0603623aa6e0d
Stephan <stephan@soundcloud.com>
2014-06-23 16:58:47 +0200

    changed from using env source file to use one test script for the whole package

--- aws/test.sh
+++ aws/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo 'Usage: "source sqs_test_bootstrap.sh" to set up environment vars for SQS test'
+# Test suite for the AWS package
 
 QUEUE_NAME_PREFIX="HlsdStaging"
 export SQS_TEST_ENDPOINT=$(aws sqs list-queues --queue-name-prefix $QUEUE_NAME_PREFIX --query QueueUrls[0] | tr -d \")
+
+go test
